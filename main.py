f55289796660e2a041b87c79e4c96d230e964e3e
durgaprasad-flm <durgaprasad@frontlinesedutech.com>
2026-03-03 00:55:09 +0530

    fix: cors

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app = FastAPI(
 # Configure CORS for frontend integration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "http://127.0.0.1:3000"],
+    allow_origins=["*"],  # Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
